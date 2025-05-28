81443d836e5f3385dd5dcde54157f826b23facb8
anooanand <anoo.anand@gmail.com>
2025-05-28 22:15:22 +1000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavBar } from './components/NavBar';
 import { HeroSection } from './components/HeroSection';
 import { FeaturesSection } from './components/FeaturesSection';
