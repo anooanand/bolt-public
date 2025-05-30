f427414a909f8e2b3d2a5e1f40c1c525cbd2b41f
anooanand <anoo.anand@gmail.com>
2025-05-31 08:05:08 +1000

    Add missing NSWSelectiveExamSimulator component

--- src/components/NSWSelectiveExamSimulator.tsx
+++ src/components/NSWSelectiveExamSimulator.tsx
@@ -10,8 +10,8 @@ export const NSWSelectiveExamSimulator: React.FC<NSWSelectiveExamSimulatorProps>
       <div className="p-6">
         <div className="flex items-center mb-4">
           <div className="bg-green-100 dark:bg-green-900/30 p-3 rounded-full mr-4">
-            <svg className="w-6 h-6 text-green-600 dark:text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            <svg className="w-6 h-6 text-green-600 dark:text-green-400\" fill="none\" stroke="currentColor\" viewBox="0 0 24 24\" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round\" strokeLinejoin="round\" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </div>
           <h3 className="text-xl font-bold text-gray-800 dark:text-white">
