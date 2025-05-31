cffe524d333a75822474e77e8bbb8148d17fbdde
anooanand <anoo.anand@gmail.com>
2025-05-31 11:23:17 +1000

    Updated OverviewTab.tsx

--- _temp_lessons_backup/lessons/day27_persuasive_language_devices/OverviewTab.tsx
+++ _temp_lessons_backup/lessons/day27_persuasive_language_devices/OverviewTab.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { TextToSpeech } from '../../../src/components/TextToSpeech';
+import { TextToSpeech } from "../../../../../src/components/TextToSpeech";
+
 
 export function OverviewTab() {
   return (
