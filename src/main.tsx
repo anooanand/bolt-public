7a3eb6322803c0033ef320378fee1ee1cc9c7967
anooanand <anoo.anand@gmail.com>
2025-06-12 08:54:11 +1000

    Fix AuthProvider context error

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './darkmode.css'; // Add this line to import the dark mode CSS
+import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
