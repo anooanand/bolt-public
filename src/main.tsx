804e78ae087481bdb8894906fc350ebb10410b8b
anooanand <anoo.anand@gmail.com>
2025-06-17 19:52:11 +1000

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './darkmode.css'; // Add this line to import the dark mode CSS
+import './darkmode.css';
 import { AuthProvider } from './contexts/AuthContext';
+import ErrorBoundary from './components/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ErrorBoundary>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
