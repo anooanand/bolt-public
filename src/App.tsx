d842febf4f13c35d24ebb9416a77e82734c65688
anooanand <anoo.anand@gmail.com>
2025-07-04 17:29:00 +1000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
-import { AuthProvider } from './contexts/AuthContext';
 import { AppProvider } from './contexts/AppContext';
-import AppContent from './components/AppContent';
+import { AppContent } from './components/AppContent';
 
 function App() {
   return (
     <ThemeProvider>
-      <AuthProvider>
-        <AppProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Navigate to="/writing" replace />} />
-              <Route path="/writing" element={<AppContent />} />
-            </Routes>
-          </Router>
-        </AppProvider>
-      </AuthProvider>
+      <AppProvider>
+        <Router>
+          <AppContent />
+        </Router>
+      </AppProvider>
     </ThemeProvider>
   );
 }
