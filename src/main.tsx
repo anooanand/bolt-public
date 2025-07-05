782466919ddc02d6035d3af286b631ecd8f2eace
anooanand <anoo.anand@gmail.com>
2025-07-05 20:21:35 +1000

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './darkmode.css';
-import { AuthProvider } from './contexts/AuthContext';
-import { LearningProvider } from './contexts/LearningContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <LearningProvider>
-        <App />
-      </LearningProvider>
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
+
