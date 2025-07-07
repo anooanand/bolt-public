85378a29379f032367c9322d89411307d87e22e7
anooanand <anoo.anand@gmail.com>
2025-07-07 23:23:22 +1000

    Updated AppContent.tsx

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -220,6 +220,7 @@ function AppContent() {
   }
 
   return (
+    <div className="kid-theme min-h-screen bg-gradient-to-br...">
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
       {/* Kid-friendly error notification */}
       {errorMessage && (
