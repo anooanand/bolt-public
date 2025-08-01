a25e01a78ab5f7f4a93dcdcb52d59101b5e24f2c
anooanand <anoo.anand@gmail.com>
2025-08-01 21:45:59 +1000

    Fix Netlify Functions proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react( )],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    proxy: {
+      '/.netlify/functions': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 });
